110df87cf5db123f490e2824957a394ff4d124ef
Gustav0172003 <gustavo.binhosantos@gmail.com>
2025-05-18 21:55:07 -0300

    gustavo 18 de maio
    adicionado parte "saiba mais" junto do menu accordion com 5 perguntas

--- js/Script.js
+++ js/Script.js
@@ -177,3 +177,16 @@ function closePopup(id) {
         airplane.style.transition = 'none';
     }, 1500);
 }
+
+
+
+// accordion
+
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(accordion => {
+    accordion.addEventListener(('click'), () =>{
+        const body = accordion.querySelector('.accordion-body');
+        body.classList.toggle('active');
+    })
+})
