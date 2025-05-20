1036ebdb128566fde962b20f21350e588b04d88b
Antonio <antoniocarlosdasilvaalves699@gmail.com>
2025-05-20 07:39:23 -0300

    função rolagem ajustada 100%

--- js/Script.js
+++ js/Script.js
@@ -73,7 +73,7 @@ swiper.on("slideChange", updateCaption); // Atualiza a cada troca de slide
 //   Fim legenda
 
 //function rolagem
-document.querySelectorAll('.acorddion-function').forEach(link => {
+document.querySelectorAll('.accordion-function').forEach(link => {
     link.addEventListener('click', function (e) {
         e.preventDefault();
 
